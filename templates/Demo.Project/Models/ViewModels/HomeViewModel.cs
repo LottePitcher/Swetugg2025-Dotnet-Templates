cb35aa40283b7c0eb1bb28d0050c1a1ef01b446f
Lotte Pitcher <github@lottepitcher.co.uk>
2025-02-04 19:27:46 +0000

    Demo Project initial commit

---
+++ templates/Demo.Project/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Demo.Project.Models.PublishedModels;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Demo.Project.Models.ViewModels
+{
+    public class HomeViewModel : Home
+    {
+        public HomeViewModel(IPublishedContent content, IPublishedValueFallback publishedValueFallback) : base(content, publishedValueFallback)
+        {
+        }
+    }
+}
